b8718959b7d4686b967ce12cc037fb06a9009f81
Rohithossain12 <https://github.com/Rohithossain12>
2025-08-18 22:58:07 +0600

    first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div className="py-16 px-4 container mx-auto">
+          <h1>About Us</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate corrupti aperiam maiores praesentium labore animi voluptatum deserunt asperiores a consectetur ea tempore adipisci corporis, delectus quis. Sed atque cum architecto animi consectetur alias perferendis. Laborum at debitis a aspernatur! Reiciendis, quae consequatur? Velit, corrupti quasi vitae maxime expedita deserunt impedit!</p>
+        </div>
+    );
+};
+
+export default About;
